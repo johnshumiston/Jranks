b4cec81f8ab643fd59ae16a90c8e34a4dfb4cf92
Joao Paulo Jung Ramia <joao.ramia@gmail.com>
2016-06-09 18:12:08 -0400

    changed drink angular

--- browser/js/drinks/drinks.js
+++ browser/js/drinks/drinks.js
@@ -4,7 +4,23 @@ app.config(function ($stateProvider) {
     $stateProvider.state('drinks', {
         url: '/drinks',
         controller: 'DrinksController',
-        templateUrl: 'js/drinks/drinks.html'
+        templateUrl: 'js/drinks/drinks.html',
+        resolve: {
+        	allDrinks: function(InventoryFactory) {
+        		return InventoryFactory.fetchByType('drink');
+        	}
+        }
+    });
+
+    $stateProvider.state('drinkItem', {
+        url: '/drink/:drinkId',
+        controller: 'DrinkController',
+        templateUrl: 'js/drink/drinkItem.html',
+        resolve: {
+          drinkItem: function (InventoryFactory, $stateParams) {
+            return InventoryFactory.fetchById($stateParams.drinkId);
+          }
+        }
     });
 
 });
