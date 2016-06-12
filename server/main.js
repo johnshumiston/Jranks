00de91e7c35bda4fa3259887d5892edd35129469
Joao Paulo Jung Ramia <joao.ramia@gmail.com>
2016-06-12 18:19:39 -0400

    created https server along with http

--- server/main.js
+++ server/main.js
@@ -1,23 +1,45 @@
 'use strict';
 var chalk = require('chalk');
 var db = require('./db');
+var fs = require('fs');
+var https = require('https');
+var app = require('./app');
+var path = require('path');
 
 // Create a node server instance! cOoL!
-var server = require('http').createServer();
+// var server = require('http').createServer();
+
+var httpsServer = https.createServer({
+  cert: fs.readFileSync(path.join(__dirname + '/cert.pem')),
+  key: fs.readFileSync(path.join(__dirname + '/key.pem'))
+}, app(db));
+
+var httpServer = require('http').createServer();
 
 var createApplication = function () {
     var app = require('./app')(db);
-    server.on('request', app); // Attach the Express application.
-    require('./io')(server);   // Attach socket.io.
+    //for http server
+    httpServer.on('request', app); // Attach the Express application.
+    require('./io')(httpServer);   // Attach socket.io.
+
+    //for https server
+    httpsServer.on('request', app); // Attach the Express application.
+    require('./io')(httpsServer);   // Attach socket.io.
 };
 
 var startServer = function () {
 
     var PORT = process.env.PORT || 1337;
 
-    server.listen(PORT, function () {
+    //for http server
+    httpServer.listen(PORT, function () {
         console.log(chalk.blue('Server started on port', chalk.magenta(PORT)));
     });
+
+    //for https server
+    httpsServer.listen(8080, function () {
+        console.log(chalk.blue('Server started on port', chalk.magenta(8080)));
+    });    
 
 };
 
