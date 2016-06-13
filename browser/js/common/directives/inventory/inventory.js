f86d5853fb093aea642d0a71b3a84df42683211a
Jonathan Schwarz <jes708@nyu.edu>
2016-06-13 16:53:49 -0400

    Update inventory.js

--- browser/js/common/directives/inventory/inventory.js
+++ browser/js/common/directives/inventory/inventory.js
@@ -28,13 +28,11 @@ app.directive('inventory', function (CartFactory, InventoryFactory, $stateParams
       
       InventoryFactory.showAddButton($stateParams.id)
       .then(function(availability){
-        console.log("AVAILABILITY: ", availability);
         scope.showAddButton = availability.data;
       })
 
       InventoryFactory.fetchReviewsById($stateParams.id)
       .then(function(reviews){
-        console.log("REVIEWS");
         scope.reviews=reviews;
         scope.newReview = {};
         scope.newReview.inventoryId = $stateParams.id;
