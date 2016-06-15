78bced6eafb68cdbe2797a44e4196c5a31112fb9
jes708 <jonschwarz@nyu.edu>
2016-06-14 20:55:12 -0400

    css done

--- browser/js/cart/cart.js
+++ browser/js/cart/cart.js
@@ -61,7 +61,10 @@ app.factory('CartFactory', function ($http, $state, $rootScope) {
     .then(function(items){
       return items.reduce(function(sum, item){
         return sum + item.qty;
-    }, 0)});
+    }, 0)})
+    .then(function(number) {
+      return number ? number : "";
+    });
   }
 
   CartFactory.getSession = function() {
